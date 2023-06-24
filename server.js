8ab8ae812ccbcd6cc16b5949152de6906cec73bc
nbayrakk <nafizbay@hotmail.com>
2023-06-24 17:50:32 +0300

    .

--- server.js
+++ server.js
@@ -46,16 +46,26 @@ async function sendMail(user) {
     });
     const a = {name: "John", age: 30, city: "New York"}
     // send mail with defined transport object
-    let info = await transporter.sendMail({
+    let mailData = {
       from: '1bilsencom@gmail.com', // sender address
       to: "nafizbayrak@gmail.com", // list of receivers
       subject: "Hello ✔", // Subject line
       text: "test", // plain text body
       html: JSON.stringify(a), // html body
+    }
+    // let info = await transporter.sendMail(mailData);
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(info);
+        }
+      });
     });
-  
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // console.log("Message sent: %s", info.messageId);
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   }
 
 app.listen(PORT,()=> {
